18e8a01435f561781149cc57e587cc2348b1d802
Loren <953481018@qq.com>
2019-01-18 09:53:03 +0800

    优化扫描预览分辨率计算核心代码

--- app/src/main/java/cn/lp/scanmode/MainActivity.java
+++ app/src/main/java/cn/lp/scanmode/MainActivity.java
@@ -1,7 +1,22 @@
 package cn.lp.scanmode;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import cn.shequren.scancode.CameraConfig;
+import cn.shequren.scancode.ScanCodeMangerUtils;
+import cn.shequren.scancode.ScanCodeMode;
+import cn.shequren.scancode.newscan.ScanCodeNewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+            }
+        });
     }
+
+
+    /**
+     * 请求授权
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestPermission() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) { //表示未授权时
+            //进行授权
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE
+                    , Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+        } else {
+            //调用打电话的方法
+            ScanCodeMangerUtils scanCodeMangerUtils = ScanCodeMangerUtils.newInstance();
+            scanCodeMangerUtils.mCode = Activity.RESULT_FIRST_USER;
+            scanCodeMangerUtils.mScanCodeMode = ScanCodeMode.ALL;
+            scanCodeMangerUtils.isNeedCahangPager = false;
+            Intent intent = new Intent(this, ScanCodeNewActivity.class);
+            intent.putExtra(CameraConfig.RESPONSE_CODE, Activity.RESULT_FIRST_USER);
+            startActivityForResult(intent, Activity.RESULT_FIRST_USER);
+          /*  ScanCodeMangerUtils.newInstance().startIntent(getActivity(),
+                    ScanCodeMode.ALL, Activity.RESULT_FIRST_USER, false);*/
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        ScanCodeMangerUtils scanCodeMangerUtils = ScanCodeMangerUtils.newInstance();
+        scanCodeMangerUtils.mCode = Activity.RESULT_FIRST_USER;
+        scanCodeMangerUtils.mScanCodeMode = ScanCodeMode.ALL;
+        scanCodeMangerUtils.isNeedCahangPager = false;
+        Intent intent = new Intent(this, ScanCodeNewActivity.class);
+        intent.putExtra(CameraConfig.RESPONSE_CODE, Activity.RESULT_FIRST_USER);
+        startActivityForResult(intent, Activity.RESULT_FIRST_USER);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Activity.RESULT_FIRST_USER && resultCode == Activity.RESULT_FIRST_USER) {
+
+            if (data != null) {
+                String resultdata = data.getStringExtra(ScanCodeMangerUtils.newInstance().resultDdataType);
+                if (!TextUtils.isEmpty(resultdata)) {
+                    Toast.makeText(MainActivity.this, resultdata, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+
+    }
+
 }
--- scan_code_model/src/main/java/cn/shequren/scancode/camera/CameraConfigurationUtils.java
+++ scan_code_model/src/main/java/cn/shequren/scancode/camera/CameraConfigurationUtils.java
@@ -4,6 +4,9 @@ import android.graphics.Point;
 import android.hardware.Camera;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -19,31 +22,48 @@ public final class CameraConfigurationUtils {
     private static final int MIN_PREVIEW_PIXELS = 480 * 320; // normal screen
     private static final double MAX_ASPECT_DISTORTION = 0.15;
 
+    /**
+     * 从相机支持的分辨率中计算出最适合的预览界面尺寸
+     *
+     * @param parameters
+     * @param screenResolution
+     * @return
+     */
     public static Point findBestPreviewSizeValue(Camera.Parameters parameters, Point screenResolution) {
-
         List<Camera.Size> rawSupportedSizes = parameters.getSupportedPreviewSizes();
         if (rawSupportedSizes == null) {
             Log.w(TAG, "Device returned no supported preview sizes; using default");
             Camera.Size defaultSize = parameters.getPreviewSize();
-            if (defaultSize == null) {
-                throw new IllegalStateException("Parameters contained no preview size!");
-            }
             return new Point(defaultSize.width, defaultSize.height);
         }
 
+        // Sort by size, descending
+        List<Camera.Size> supportedPreviewSizes = new ArrayList<Camera.Size>(rawSupportedSizes);
+        Collections.sort(supportedPreviewSizes, new Comparator<Camera.Size>() {
+            @Override
+            public int compare(Camera.Size a, Camera.Size b) {
+                int aPixels = a.height * a.width;
+                int bPixels = b.height * b.width;
+                if (bPixels < aPixels) {
+                    return -1;
+                }
+                if (bPixels > aPixels) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
         if (Log.isLoggable(TAG, Log.INFO)) {
             StringBuilder previewSizesString = new StringBuilder();
-            for (Camera.Size size : rawSupportedSizes) {
-                previewSizesString.append(size.width).append('x').append(size.height).append(' ');
+            for (Camera.Size supportedPreviewSize : supportedPreviewSizes) {
+                previewSizesString.append(supportedPreviewSize.width).append('x').append(supportedPreviewSize.height).append(' ');
             }
             Log.i(TAG, "Supported preview sizes: " + previewSizesString);
         }
 
-        double screenAspectRatio = screenResolution.x / (double) screenResolution.y;
-
-        // Find a suitable size, with max resolution
-        int maxResolution = 0;
         Camera.Size maxResPreviewSize = null;
+        int diff = Integer.MAX_VALUE;
         for (Camera.Size size : rawSupportedSizes) {
             int realWidth = size.width;
             int realHeight = size.height;
@@ -55,11 +75,11 @@ public final class CameraConfigurationUtils {
             boolean isCandidatePortrait = realWidth < realHeight;
             int maybeFlippedWidth = isCandidatePortrait ? realHeight : realWidth;
             int maybeFlippedHeight = isCandidatePortrait ? realWidth : realHeight;
-            double aspectRatio = maybeFlippedWidth / (double) maybeFlippedHeight;
-            double distortion = Math.abs(aspectRatio - screenAspectRatio);
-            if (distortion > MAX_ASPECT_DISTORTION) {
-                continue;
-            }
+            //            double aspectRatio = maybeFlippedWidth / (double) maybeFlippedHeight;
+            //            double distortion = Math.abs(aspectRatio - screenAspectRatio);
+            //            if (distortion > MAX_ASPECT_DISTORTION) {
+            //                continue;
+            //            }
 
             if (maybeFlippedWidth == screenResolution.x && maybeFlippedHeight == screenResolution.y) {
                 Point exactPoint = new Point(realWidth, realHeight);
@@ -67,31 +87,43 @@ public final class CameraConfigurationUtils {
                 return exactPoint;
             }
 
-            // Resolution is suitable; record the one with max resolution
-            if (resolution > maxResolution) {
-                maxResolution = resolution;
+            int newDiff = Math.abs(maybeFlippedWidth - screenResolution.x) + Math.abs(maybeFlippedHeight - screenResolution.y);
+            if (newDiff < diff) {
                 maxResPreviewSize = size;
+                diff = newDiff;
             }
+
+            // Resolution is suitable; record the one with max resolution
+            //            if (resolution > maxResolution) {
+            //                maxResolution = resolution;
+            //                maxResPreviewSize = size;
+            //            }
         }
 
-        // If no exact match, use largest preview size. This was not a great idea on older devices because
-        // of the additional computation needed. We're likely to get here on newer Android 4+ devices, where
-        // the CPU is much more powerful.
         if (maxResPreviewSize != null) {
             Point largestSize = new Point(maxResPreviewSize.width, maxResPreviewSize.height);
             Log.i(TAG, "Using largest suitable preview size: " + largestSize);
             return largestSize;
         }
 
+        // If no exact match, use largest preview size. This was not a great
+        // idea on older devices because
+        // of the additional computation needed. We're likely to get here on
+        // newer Android 4+ devices, where
+        // the CPU is much more powerful.
+        if (!supportedPreviewSizes.isEmpty()) {
+            Camera.Size largestPreview = supportedPreviewSizes.get(0);
+            Point largestSize = new Point(largestPreview.width, largestPreview.height);
+            Log.i(TAG, "Using largest suitable preview size: " + largestSize);
+            return largestSize;
+        }
+
         // If there is nothing at all suitable, return current preview size
         Camera.Size defaultPreview = parameters.getPreviewSize();
-        if (defaultPreview == null) {
-            throw new IllegalStateException("Parameters contained no preview size!");
-        }
         Point defaultSize = new Point(defaultPreview.width, defaultPreview.height);
         Log.i(TAG, "No suitable preview sizes, using default: " + defaultSize);
+
         return defaultSize;
     }
-
 
 }
